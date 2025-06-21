56d0d8417f5e69b2c53d7cbc8cd378954973ea44
sasha <sashatabachukpro@gmail.com>
2025-06-21 22:20:24 +0000

    Лабораторна 13. Ланцюги завдань (Chain)

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Blog\Admin\PostController;
 use App\Http\Controllers\RestTestController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DiggingDeeperController;
+use App\Http\Controllers\Blog\Admin\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,18 +21,21 @@ Route::middleware([
 });
 
 Route::resource('rest', RestTestController::class)->names('restTest');
-
 Route::group(['prefix' => 'digging_deeper'], function () {
+    Route::get('process-video', [DiggingDeeperController::class, 'processVideo'])
+        ->name('digging_deeper.processVideo');
+
+    Route::get('prepare-catalog', [DiggingDeeperController::class, 'prepareCatalog'])
+        ->name('digging_deeper.prepareCatalog');
 
     Route::get('collections', [DiggingDeeperController::class, 'collections'])
-
         ->name('digging_deeper.collections');
-
 });
+
+
 Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
 });
-
 //Адмінка
 $groupData = [
     'namespace' => 'App\Http\Controllers\Blog\Admin',
